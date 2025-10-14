a3d4957202077300bfd9f10c59440dfa6cf763b1
dar3v <un.finished342@slmail.me>
2025-10-15 05:18:19 +0800

    ServerUser init object structure

--- src/main/java/sniffle/ServerUser.java
+++ src/main/java/sniffle/ServerUser.java
@@ -1,5 +1,38 @@
 package sniffle;
 
+import java.net.*;
+import java.security.*;
+import java.util.*;
+
 public class ServerUser extends User {
-    // TODO code here
+    private int port;
+    private ServerSocket serverSocket;
+    private final List<ClientHandler> clients = new ArrayList<>();
+
+    public ServerUser(String username, int port) {
+        super(username);
+        this.port = port;
+    }
+
+    private void setPort(int port) {
+        // TODO some excepton handling perhaps..
+        this.port = port;
+    }
+
+    @Override
+    public void login() {
+        // TODO code here
+    }
+
+    public void startServer() {
+        // TODO code here
+    }
+
+    public synchronized void broadcast(String plain, ClientHandler sender) {
+        // TODO code here
+    }
+
+    public synchronized void removeClient(ClientHandler c) { clients.remove(c); }
+    public PublicKey getPublicKey() { return publicKey; }
+    public PrivateKey getPrivateKey() { return privateKey; }
 }
