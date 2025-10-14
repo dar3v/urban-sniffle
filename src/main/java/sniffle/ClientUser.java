9ce0abf98d241edc83d46923ce2ae213f6a46a1a
dar3v <un.finished342@slmail.me>
2025-10-15 05:15:57 +0800

    ClientUser init object structure

--- src/main/java/sniffle/ClientUser.java
+++ src/main/java/sniffle/ClientUser.java
@@ -1,5 +1,31 @@
 package sniffle;
 
 public class ClientUser extends User {
-    // TODO code here
+    private String serverIP;
+    private int port;
+
+    public ClientUser(String username, String serverIP, int port) {
+        super(username);
+        setServerIP(serverIP);
+        setPort(port);
+    }
+
+    private void setServerIP(String serverIP) {
+        // TODO some sort of exception handling perhaps...
+        this.serverIP = serverIP;
+    }
+
+    public void setPort(int port) {
+        // TODO some sort of exception handling perhaps...
+        this.port = port;
+    }
+
+    @Override
+    public void login() {
+        // TODO code here
+    }
+
+    public void connectToServer() {
+        // TODO code here
+    }
 }
