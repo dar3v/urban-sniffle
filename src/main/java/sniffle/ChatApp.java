a0d4507be64cc4ec89770a5faeb544b465ab80d2
dar3v <un.finished342@slmail.me>
2025-10-14 23:45:58 +0800

    rename main class

--- src/main/java/sniffle/ChatApp.java
+++
@@ -1,7 +0,0 @@
-package sniffle;
-
-public class ChatApp {
-    public static void main(String[] args) {
-        // TODO code here
-    }
-}
---
+++ src/main/java/sniffle/UrbanSniffle.java
@@ -0,0 +1,8 @@
+package sniffle;
+
+public class UrbanSniffle {
+    public static void main(String[] args) {
+        // TODO code here yes
+        System.out.println("Development in progress...");
+    }
+}
